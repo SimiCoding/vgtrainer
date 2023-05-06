c84db49cbcf9f95e87ae23b88dcde24743594887
Simon <simi.coding@gmail.com>
2023-05-06 14:15:26 +0200

    deleting test file

--- src/data/fileStructure/jsStructure.js
+++ src/data/fileStructure/jsStructure.js
@@ -1,11 +1,11 @@
 // All coded by SimiCoding on github!
 export const jsStructure = [
-  {
+  /*  {
     folder: `test/`,
     file: `test.js`,
     type: `module`, // You have to set type on module if you export a function
   },
-/*{
+{
     folder: `main/type-text/`,
     file: `erase.js`,
     type: `module`, // You have to set type on module if you export a function
--- src/js/test/test.js
+++
@@ -1,4 +0,0 @@
-// All coded by SimiCoding on github!
-export function test(test) {
-    console.log(`This is a test: ${test}`);
-}
